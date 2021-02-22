af36617b173f188926c51251dd22dfa69ac95095
EduardRo <eduardstancu@yahoo.com>
2021-02-23 00:47:37 +0200

    first commit

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,24 +4,28 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  address public minter;
+    address public minter;
+    // add minter changed event
+    event MinterChanged(address indexed from, address to);
 
-  event MinterChanged(address indexed from, address to);
+    constructor() public payable ERC20("Decentralized Bank Currency", "DCB") {
+        // asign initial minter
+        minter = msg.sender;
+    }
 
-  constructor() public payable ERC20("Decentralized Bank Currency", "DBC") {
-    minter = msg.sender; //only initially
-  }
+    function passMinterRole(address dBank) public returns (bool) {
+        require(msg.sender == minter,'Error only owner can change pass minter role');
+        minter = dBank;
+        emit MinterChanged(msg.sender, dBank);
+        return true;
+    }
 
-  function passMinterRole(address dBank) public returns (bool) {
-  	require(msg.sender==minter, 'Error, only owner can change pass minter role');
-  	minter = dBank;
-
-    emit MinterChanged(msg.sender, dBank);
-    return true;
-  }
-
-  function mint(address account, uint256 amount) public {
-		require(msg.sender==minter, 'Error, msg.sender does not have minter role'); //dBank
-		_mint(account, amount);
-	}
+    // Add pass minter role function
+    function mint(address account, uint256 amount) public {
+        require(
+            msg.sender == minter,
+            "Error, msg.sender does not have minter role"
+        );
+        _mint(account, amount);
+    }
 }
